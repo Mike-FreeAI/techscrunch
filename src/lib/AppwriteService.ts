2c078ce47aa197ef10952e3164c67d53c79894ff
Matej Bačo <matejbaco2000@gmail.com>
2023-03-24 08:25:37 +0100

    Add promoted articles

--- src/lib/AppwriteService.ts
+++ src/lib/AppwriteService.ts
@@ -29,6 +29,7 @@ export type Article = {
 	imageId: string;
 
 	// Front-end only
+	promoted?: boolean;
 	category?: Category;
 	author?: Author;
 	verboseDate: string;
@@ -68,11 +69,31 @@ export const AppwriteService = {
 	getAuthor: async (articleId: string) => {
 		const response = await databases.listDocuments<Author>('default', 'authors', [
 			Query.limit(1),
-			Query.orderDesc('$id'),
 			Query.equal('articleId', articleId)
 		]);
 
 		return response.documents[0] ?? undefined;
+	},
+	isPromoted: async (articleId: string) => {
+		const response = await databases.listDocuments<Author>('default', 'promotions', [
+			Query.limit(1),
+			Query.equal('articleId', articleId)
+		]);
+
+		return response.documents.length > 0;
+	},
+	arePromoted: async (articleIds: string[]) => {
+		const response = await databases.listDocuments<Author>('default', 'promotions', [
+			Query.equal('articleId', articleIds)
+		]);
+
+		const obj: { [key: string]: boolean } = {};
+
+		for(const articleId of articleIds) {
+			obj[articleId] = response.documents.find((promotion) => promotion.articleId === articleId) ? true : false;
+		}
+
+		return obj;
 	},
 	getAuthors: async (articleIds: string[]) => {
 		return await databases.listDocuments<Author>('default', 'authors', [
@@ -84,13 +105,15 @@ export const AppwriteService = {
 
 		article.verboseDate = getVerboseDate(article.$createdAt);
 
-		const [ category, author ] = await Promise.all([
+		const [ category, author, promoted ] = await Promise.all([
 			await AppwriteService.getCategory(article.categoryId),
 			await AppwriteService.getAuthor(article.$id),
+			await AppwriteService.isPromoted(article.$id)
 		]);
 
 		article.category = category;
 		article.author = author;
+		article.promoted = promoted;
 
 		return article;
 	},
@@ -113,7 +136,7 @@ export const AppwriteService = {
 		const categoryIds = [...new Set(articles.documents.map((article) => article.categoryId))];
 		const articleIds = [...new Set(articles.documents.map((article) => article.$id))];
 
-		const [ categories, authors ] = await Promise.all([
+		const [ categories, authors, promotions ] = await Promise.all([
 			(async () => {
 				if (categoryIds.length > 0) {
 					return (await AppwriteService.getCategories(categoryIds)).documents;
@@ -128,6 +151,14 @@ export const AppwriteService = {
 				}
 
 				return [];
+			})(),
+
+			(async () => {
+				if (articleIds.length > 0) {
+					return await AppwriteService.arePromoted(articleIds);
+				}
+
+				return {};
 			})()
 		]);
 
@@ -136,6 +167,7 @@ export const AppwriteService = {
 				...article,
 				category: categories.find((category) => category.$id === article.categoryId),
 				author: authors.find((author) => author.articleId === article.$id),
+				promoted: promotions[article.$id]
 			};
 		});
 
--- src/lib/components/GridArticle.svelte
+++ src/lib/components/GridArticle.svelte
@@ -1,30 +1,64 @@
 <script lang="ts">
-	import { AppwriteService, type Article } from "$lib/AppwriteService";
+	import { AppwriteService, type Article } from '$lib/AppwriteService';
 
-  export let article: Article;
+	export let article: Article;
 </script>
 
 <li class="articles-grid-item">
-    <article class="articles-grid-item-article">
-        <a href={`/article/${article.$id}`} class="articles-grid-item-link">
-            <header class="articles-grid-item-header">
-                <a href={`/category/${article.categoryId}`} class="category">{article.category?.name ?? 'Unknown Category'}</a>
-                <h3 class="heading-level-2">{article.title}</h3>
-                <a href={`/author/${article.author?.$id}`} class="author u-margin-block-start-12">
-                    By <span>{article.author?.name ?? 'Unknown Author'}</span>
-                </a>
-                <time class="date-time">{article.verboseDate}</time>
-            </header>
-            <p class="articles-grid-item-content u-trim-6 u-cross-child-start">
-                {article.content}
-            </p>
-            <figure class="articles-grid-item-image">
-                <img
-                    class="u-aspect-16-9 u-margin-block-start-4"
-                    src={AppwriteService.getThumbnail(article.imageId, 500, 500)}
-                    alt=""
-                />
-            </figure>
-        </a>
-    </article>
+	{#if !article.promoted}
+		<article class="articles-grid-item-article">
+			<a href={`/article/${article.$id}`} class="articles-grid-item-link">
+				<header class="articles-grid-item-header">
+					<a href={`/category/${article.categoryId}`} class="category"
+						>{article.category?.name ?? 'Unknown Category'}</a
+					>
+					<h3 class="heading-level-2 category-heading">{article.title}</h3>
+					<a href={`/author/${article.author?.$id}`} class="author u-margin-block-start-12">
+						By <span>{article.author?.name ?? 'Unknown Author'}</span>
+					</a>
+					<time class="date-time">{article.verboseDate}</time>
+				</header>
+				<p class="articles-grid-item-content u-trim-6 u-cross-child-start">
+					{article.content}
+				</p>
+				<figure class="articles-grid-item-image">
+					<img
+						class="u-aspect-16-9 u-margin-block-start-4"
+						src={AppwriteService.getThumbnail(article.imageId, 500, 500)}
+						alt=""
+					/>
+				</figure>
+			</a>
+		</article>
+        {:else}
+
+        <article class="articles-grid-item-article">
+			<a href={`/article/${article.$id}`} class="articles-grid-item-link">
+				<header class="articles-grid-item-header">
+					<p class="category-featured"
+						>Featured Article</p
+					>
+
+                    <h3 class="heading-level-2 category-heading">{article.title}</h3>
+
+                    <p class="u-trim-6 category-content">{article.content}</p>
+
+                  
+					<a href={`/author/${article.author?.$id}`} class="author u-margin-block-start-12">
+						By <span>{article.author?.name ?? 'Unknown Author'}</span>
+					</a>
+					<time class="date-time">{article.verboseDate}</time>
+				</header>
+				<figure class="articles-grid-item-image">
+					<!-- TODO: Stretch -->
+					<img
+						class="u-aspect-16-9 u-margin-block-start-4"
+						src={AppwriteService.getThumbnail(article.imageId, 500, 500)}
+						alt=""
+					/>
+				</figure>
+			</a>
+		</article>
+
+	{/if}
 </li>
