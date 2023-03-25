d640ba925dd9f492f93729641d837a5d2631f4ee
Elad Shechter <elad2412@gmail.com>
2023-03-25 11:43:45 +0900

    Login Password page - Make HTML demo - update some styles

--- src/routes/(simple)/auth/login-password/+page.svelte
+++ src/routes/(simple)/auth/login-password/+page.svelte
@@ -1,1 +1,36 @@
-<p>Login Page - Step2</p>
+<script lang="ts">
+  import '$lib/styles/index.scss';
+</script>
+
+<div class="page-grid-2-1">
+  <div class="page-grid-2-1-column-1">
+
+  </div>
+  <div class="page-grid-2-1-column-2 u-flex-vertical u-main-center">
+    <div class="u-max-width-400 u-width-full-line u-padding-inline-24 u-padding-block-start-28 u-margin-inline-auto">
+
+      <img src="/img/techscrunch-logo.svg" class="u-block u-margin-inline-auto" alt="Techs Crunch" width="70">
+      <address class="u-block u-text-center u-margin-block-start-16">some-email@gmail.com</address>
+      <h1 class="heading-level-2 u-margin-block-start-16 u-text-center">Enter password</h1>
+      <p class="u-text-center">fo Finish sign in</p>
+
+      <form class="form u-margin-block-start-64">
+        <ul class="form-list">
+          <li class="form-item">
+            <input class="input-text" type="password" placeholder="Password" aria-label="Password">
+          </li>
+        </ul>
+
+        <button class="button u-margin-block-start-24 u-width-full-line">
+          <span class="text">Next</span>
+        </button>
+
+        <div class="u-margin-block-start-24">
+          <a class="link u-block u-width-fit-content u-margin-inline-auto" href="">Forgot password?</a>
+        </div>
+
+      </form>
+
+    </div>
+  </div>
+</div>
