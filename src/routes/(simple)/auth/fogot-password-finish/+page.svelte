29cb9029d41f5527692e1607e6b944284eccf9ef
Elad Shechter <elad2412@gmail.com>
2023-03-25 21:18:08 +0900

    Forgot Password Finish page - Make HTML demo - + small content fix in another page

--- src/routes/(simple)/auth/fogot-password-finish/+page.svelte
+++ src/routes/(simple)/auth/fogot-password-finish/+page.svelte
@@ -1,1 +1,29 @@
-<p>Forgot Password Page - Step2</p>
+<script lang="ts">
+  import '$lib/styles/index.scss';
+</script>
+
+<div class="page-grid-2-1">
+  <div class="page-grid-2-1-column-1">
+
+  </div>
+  <div class="page-grid-2-1-column-2 u-flex-vertical u-main-center">
+    <div class="u-max-width-400 u-width-full-line u-padding-inline-24 u-padding-block-start-28 u-margin-inline-auto">
+
+      <img src="/img/techscrunch-logo.svg" class="u-block u-margin-inline-auto" alt="Techs Crunch" width="70">
+
+      <h1 class="heading-level-2 u-margin-block-start-16 u-text-center">Verification email sent</h1>
+      <address class="u-block u-text-center u-margin-block-start-16">to some-email@gmail.com</address>
+
+      <p class="u-margin-block-start-48 u-bold">Click on secure likn in the email to approve your sign-in request</p>
+
+      <p class="u-margin-block-start-32 u-text-center">Resend verification email in <span>55</span></p>
+
+      <a class="link u-block u-width-fit-content u-margin-inline-auto u-margin-block-start-16" href="">
+        Try another way to sign in
+      </a>
+
+
+
+    </div>
+  </div>
+</div>
