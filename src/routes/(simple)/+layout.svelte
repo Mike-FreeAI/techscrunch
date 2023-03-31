1c39a7934eb40db597ea7ccfe518e838d66cf356
Matej Bačo <matejbaco2000@gmail.com>
2023-03-31 19:15:18 +0200

    Improve auth layout, add articles to profile

--- src/routes/(simple)/+layout.svelte
+++ src/routes/(simple)/+layout.svelte
@@ -4,7 +4,9 @@
 </script>
 
 <div class="page-grid-2-1">
-	<div class="page-grid-2-1-column-1" />
+	<div class="page-grid-2-1-column-1">
+		<img src="/img/auth-cover.webp" alt="">
+	</div>
 	<div class="page-grid-2-1-column-2 u-flex-vertical u-main-center">
 		<div
 			class="u-max-width-400 u-width-full-line u-padding-inline-24 u-padding-block-start-28 u-margin-inline-auto"
